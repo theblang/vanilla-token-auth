6aa3ea000433cc595cbe57231d83ee0d139f3a44
Matt Langston <matt@pedago.com>
2022-08-16 11:46:06 -0500

    Improve fetch existance check to avoid a not defined error

--- vanilla-token-auth.js
+++ vanilla-token-auth.js
@@ -168,8 +168,8 @@ const configs = {
             wechat_official_account: '/auth/wechat_official_account',
         },
 
-        // Default to fetch, but don't assume it's available (e.g., when doing SSR)
-        httpWrapper: typeof window !== `undefined` ? fetch : undefined,
+        // Default to fetch, but don't assume it's available (e.g., in Jest or when doing SSR)
+        httpWrapper: window?.fetch != null ? window.fetch : undefined,
 
         broadcast(name, payload) {
             if (process.env.NODE_ENV === 'development') {
